a44cd38fd9fa3301d23471d0c8138b14ba301989
Kim Uhjin <ajin0714@imagoworks.ai>
2023-09-02 22:49:32 +0900

    feat: board page expand 기능 추가

--- src/Composition/SideBar/SideBar.jsx
+++ src/Composition/SideBar/SideBar.jsx
@@ -1,29 +1,20 @@
 import { Button, Stack } from "@mui/material";
-import {
-  BsClipboardData,
-  BsBoxArrowInLeft,
-  BsBoxArrowInRight,
-} from "react-icons/bs";
+import { BsClipboardData } from "react-icons/bs";
 import { RiTeamFill } from "react-icons/ri";
 import { FaUserFriends } from "react-icons/fa";
 import { BOARD, TEAM, WORKER } from "../../Constant/route";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
 
 export const SideBar = () => {
-  const [isSideBarNarrow, setIsSideBarNarrow] = useState(false);
-  const onSidebarSizeButtonClick = () => {
-    setIsSideBarNarrow((prev) => !prev);
-  };
   return (
     <Stack
       sx={{
-        width: isSideBarNarrow ? "30px" : "250px",
+        width: "250px",
         height: "100%",
         borderRadius: "12px",
         backgroundColor: "#303030",
         marginRight: "16px",
-        padding: isSideBarNarrow ? "0px" : "16px 16px 32px 16px",
+        padding: "16px 16px 32px 16px",
         flexDirection: "column",
         justifyContent: "space-between",
         alignItems: "center",
@@ -35,61 +26,30 @@ export const SideBar = () => {
             padding: "6px 0px",
             width: "100%",
             flexDirection: "row",
-            justifyContent: isSideBarNarrow ? "center" : "flex-end",
+            justifyContent: "flex-end",
             alignItems: "center",
           }}
-        >
-          <Button
-            variant="contained"
-            sx={{
-              width: "22px",
-              minWidth: "22px",
-              height: "22px",
-              padding: "0px",
-              backgroundColor: "transparent",
-              boxShadow: "none",
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
-              ":hover": {
-                backgroundColor: "#303030",
-              },
-            }}
-            onClick={onSidebarSizeButtonClick}
-          >
-            {isSideBarNarrow ? (
-              <BsBoxArrowInRight color="#fff" size={"18px"} />
-            ) : (
-              <BsBoxArrowInLeft color="#fff" size={"18px"} />
-            )}
-          </Button>
-        </Stack>
-        {!isSideBarNarrow && (
-          <>
-            <NavigateButton route={BOARD}>
-              <BsClipboardData size={"18px"} />
-              Board
-            </NavigateButton>
-            <NavigateButton route={WORKER}>
-              <FaUserFriends size={"18px"} />
-              Worker
-            </NavigateButton>
-            <NavigateButton route={TEAM}>
-              <RiTeamFill size={"18px"} />
-              Team
-            </NavigateButton>
-          </>
-        )}
+        ></Stack>
+
+        <NavigateButton route={BOARD}>
+          <BsClipboardData size={"18px"} />
+          Board
+        </NavigateButton>
+        <NavigateButton route={WORKER}>
+          <FaUserFriends size={"18px"} />
+          Worker
+        </NavigateButton>
+        <NavigateButton route={TEAM}>
+          <RiTeamFill size={"18px"} />
+          Team
+        </NavigateButton>
       </Stack>
 
-      {!isSideBarNarrow && (
-        <Stack
-          component={"img"}
-          src="./images/logo.png"
-          sx={{ width: "150px" }}
-        />
-      )}
+      <Stack
+        component={"img"}
+        src="./images/logo.png"
+        sx={{ width: "150px" }}
+      />
     </Stack>
   );
 };
--- src/Page/Board.jsx
+++ src/Page/Board.jsx
@@ -1,34 +1,72 @@
-import { MenuItem, Select, Stack } from "@mui/material";
+import { Button, IconButton, MenuItem, Select, Stack } from "@mui/material";
 import React from "react";
 import { Header } from "../Composition/Board/Header";
 import { BoardsContainer } from "../Composition/Board/BoardsContainer";
 import { useState } from "react";
-
+import { BiExpand, BiCollapse } from "react-icons/bi";
 const Board = () => {
   const [selectedBoardId, setSelectedBoardId] = useState("1ks_4mf");
+  const [isExpand, setIsExpand] = useState(false);
+  const onExpandButtonClick = () => {
+    setIsExpand((prev) => !prev);
+  };
   return (
-    <Stack sx={{ width: "100%", height: "100%", padding: "8px" }}>
+    <Stack
+      sx={{
+        width: "100%",
+        height: "100%",
+        padding: "8px",
+        backgroundColor: "#fff",
+        overflow: "scroll",
+        ...(isExpand
+          ? { position: "fixed", zIndex: 2, left: 0, top: 0, padding: "2px" }
+          : {}),
+      }}
+    >
       {/* <Header /> */}
-      <Select
-        sx={{ width: "300px", height: "36px" }}
-        value={selectedBoardId}
-        onChange={(e) => {
-          setSelectedBoardId(e.target.value);
+      <Stack
+        sx={{
+          width: "100%",
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          alignItems: "center",
         }}
       >
-        <MenuItem value="1ks_4mf">{`0th floor (1KS & 4MF)`}</MenuItem>
-        <MenuItem value="ofd">{`0th floor  (OFD)`}</MenuItem>
-        <MenuItem value="5sr">{`clean room (5SR)`}</MenuItem>
-        <MenuItem value="3cl">{`clean room (3CL)`}</MenuItem>
-        <MenuItem value="ren">{`1st floor (Ren)`}</MenuItem>
-        <MenuItem value="smt">{`smt  (SMT)`}</MenuItem>
-      </Select>
+        <Select
+          sx={{ width: "300px", height: "36px" }}
+          value={selectedBoardId}
+          onChange={(e) => {
+            setSelectedBoardId(e.target.value);
+          }}
+        >
+          <MenuItem value="1ks_4mf">{`0th floor (1KS & 4MF)`}</MenuItem>
+          <MenuItem value="ofd">{`0th floor  (OFD)`}</MenuItem>
+          <MenuItem value="5sr">{`clean room (5SR)`}</MenuItem>
+          <MenuItem value="3cl">{`clean room (3CL)`}</MenuItem>
+          <MenuItem value="ren">{`1st floor (Ren)`}</MenuItem>
+          <MenuItem value="smt">{`smt  (SMT)`}</MenuItem>
+        </Select>
+        <IconButton
+          sx={{
+            marginLeft: "16px",
+            width: "30px",
+            height: "30px",
+          }}
+          onClick={onExpandButtonClick}
+        >
+          {isExpand ? (
+            <BiCollapse size={"24px"} color="#333333" />
+          ) : (
+            <BiExpand size={"24px"} color="#333333" />
+          )}
+        </IconButton>
+      </Stack>
+
       <Stack
         sx={{
           width: "100%",
           height: "100%",
           marginTop: "16px",
-
           flexDirection: "row",
           gap: "8px",
         }}
