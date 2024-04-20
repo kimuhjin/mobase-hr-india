9d5ad4fe7abbf7dcc96fe2edc657aa726d4d33c4
Kim Uhjin <ajin0714@imagoworks.ai>
2024-04-20 12:23:34 +0900

    feat: getColor optionalChaining 수정

--- src/Util/getColor.js
+++ src/Util/getColor.js
@@ -1,16 +1,16 @@
 export const getColor = (company) => {
-  console.log(company.toLowerCase());
-  if (company.toLowerCase() === "mobase")
+  const company_ = company?.toLowerCase();
+  if (company_ === "mobase")
     return {
       backgroundColor: "#0026e1",
       color: "#fff",
     };
-  else if (company.toLowerCase() === "get")
+  else if (company_ === "get")
     return {
       backgroundColor: "#99b8d7",
       color: "#000",
     };
-  else if (company.toLowerCase() === "cl")
+  else if (company_ === "cl")
     return {
       backgroundColor: "#d5e6f7",
       color: "#000",
